71a53d0eff54c34e3064ee7cf91fd055a09e54d7
Mulya Tri Atmaja <mulyaatmaja604@gmail.com>
2024-06-28 20:52:19 +0700

    add show password :mulya

--- src/screen/LoginScreen.js
+++ src/screen/LoginScreen.js
@@ -1,44 +1,46 @@
 /* eslint-disable prettier/prettier */
 import React, { useState } from 'react';
-import  AsyncStorage  from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const LoginScreen = ({ navigation }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [secureTextEntry, setSecureTextEntry] = useState(true);
 
     const handleLogin = async () => {
         try {
-          const response = await fetch('https://be-rent-car.vercel.app/users/login', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              email: email, // Ganti dengan username yang sesuai
-              password: password, // Ganti dengan password yang sesuai
-            }),
-          });
-      
-          if (response.ok) {
-            const data = await response.json();
-                    
-            // Simpan token ke AsyncStorage
-            await AsyncStorage.setItem('@myApp:token', data.accessToken);
-            await AsyncStorage.setItem('@myApp:name', data.name);
-      
-            // Ganti halaman ke 'Main' setelah berhasil login
-            navigation.replace('Main');
-          } else {
-            console.error('Login failed');
-          }
-        } catch (error) {
-          console.error('Error during login:', error);
-        }
-      };
-      
+            const response = await fetch('https://be-rent-car.vercel.app/users/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                }),
+            });
 
-    
+            if (response.ok) {
+                const data = await response.json();
+
+                await AsyncStorage.setItem('@myApp:token', data.accessToken);
+                await AsyncStorage.setItem('@myApp:name', data.name);
+
+                navigation.replace('Main');
+            } else {
+                console.error('Login failed');
+            }
+        } catch (error) {
+            console.error('Error during login:', error);
+        }
+    };
+
+    const toggleSecureEntry = () => {
+        setSecureTextEntry(!secureTextEntry);
+    };
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Login</Text>
@@ -51,14 +53,19 @@ const LoginScreen = ({ navigation }) => {
                 keyboardType="email-address"
                 autoCapitalize="none"
             />
-            <TextInput
-                style={styles.input}
-                placeholder="Password"
-                placeholderTextColor="#aaa"
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry
-            />
+            <View style={styles.passwordContainer}>
+                <TextInput
+                    style={styles.passwordInput}
+                    placeholder="Password"
+                    placeholderTextColor="#aaa"
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry={secureTextEntry}
+                />
+                <TouchableOpacity onPress={toggleSecureEntry} style={styles.eyeIcon}>
+                    <Icon name={secureTextEntry ? 'eye-slash' : 'eye'} size={20} color="#aaa" />
+                </TouchableOpacity>
+            </View>
             <TouchableOpacity style={styles.button} onPress={handleLogin}>
                 <Text style={styles.buttonText}>Login</Text>
             </TouchableOpacity>
@@ -95,6 +102,25 @@ const styles = StyleSheet.create({
         fontSize: 16,
         backgroundColor: '#f9f9f9',
         color: '#000'
+    },
+    passwordContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderColor: '#ddd',
+        borderWidth: 1,
+        borderRadius: 10,
+        marginBottom: 16,
+        backgroundColor: '#f9f9f9',
+    },
+    passwordInput: {
+        flex: 1,
+        height: 50,
+        paddingHorizontal: 16,
+        fontSize: 16,
+        color: '#000',
+    },
+    eyeIcon: {
+        padding: 10,
     },
     button: {
         backgroundColor: '#007BFF',
