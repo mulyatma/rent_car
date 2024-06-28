747c07e528cded274ca580fb47021cccf335a82d
Mulya Tri Atmaja <mulyaatmaja604@gmail.com>
2024-06-28 21:41:06 +0700

    ubah navbar : mulya

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import Home from './src/screen/Home';
 import SplashScreen from './src/screen/SplashScreen';
 import SearchScreen from './src/screen/Search';
-import Menu from './src/screen/Profile';
+import Menu from './src/screen/Rent';
 import LoginScreen from './src/screen/LoginScreen';
 import RegisterScreen from './src/screen/RegisterScreen';
 import DetailCar from './src/screen/DetailCar';
@@ -20,19 +20,19 @@ function MainTabs() {
   return (
     <Tab.Navigator>
       <Tab.Screen
-        name="Home"
+        name="Beranda"
         component={Home}
         options={{ tabBarIcon: ({ color, size }) => (<Icon name="home-outline" color={color} size={size} />), headerShown: false }}
       />
       <Tab.Screen
-        name="Search"
+        name="Cari"
         component={SearchScreen}
         options={{ tabBarIcon: ({ color, size }) => (<Icon name="search-outline" color={color} size={size} />), headerShown: false }}
       />
       <Tab.Screen
-        name="Profile"
+        name="Sewa"
         component={Menu}
-        options={{ tabBarIcon: ({ color, size }) => (<Icon name="person-outline" color={color} size={size} />), headerShown: false }}
+        options={{ tabBarIcon: ({ color, size }) => (<Icon name="car-outline" color={color} size={size} />), headerShown: false }}
       />
     </Tab.Navigator>
   );
