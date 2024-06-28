a3e7c48f523f54f1ec4f3f130f5e9389f649850b
Mulya Tri Atmaja <mulyaatmaja604@gmail.com>
2024-06-29 01:21:36 +0700

    fixing image

--- src/components/CardCarList.js
+++ src/components/CardCarList.js
@@ -60,6 +60,7 @@ const styles = StyleSheet.create({
         width: 100,
         height: '80%',
         borderRadius: 8,
+        resizeMode: 'contain',
     },
     cardDetails: {
         flex: 1,
