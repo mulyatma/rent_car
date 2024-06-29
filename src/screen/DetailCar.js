fd90e6d11bd09fc8aa29ee6a0115b9be7340a41f
Mulya Tri Atmaja <mulyaatmaja604@gmail.com>
2024-06-29 17:28:01 +0700

    add scrollview

--- src/screen/DetailCar.js
+++ src/screen/DetailCar.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity, Alert, Modal } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Alert, Modal, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import DatePicker from 'react-native-date-picker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -80,131 +80,131 @@ const DetailCar = ({ route, navigation }) => {
     return (
         <View style={styles.container}>
             <Image source={{ uri: car.img }} style={styles.image} />
-            <View style={styles.wrapper}>
-                <Text style={styles.name}>{car.nameCar}</Text>
-                <View style={styles.datePickerSection}>
-                    <Text style={styles.datePickerSectionText} >Atur tanggal sewa</Text>
-                    <View style={styles.datePickerRow}>
-                        <View style={styles.datePickerWrapper}>
-                            {/* <Text style={styles.datePickerLabel}>Start</Text> */}
-                            <TouchableOpacity onPress={() => setShowStartPicker(true)} style={styles.datePickerButton}>
-                                <Text style={styles.datePickerButtonText}>{formatDate(startDate)}</Text>
-                            </TouchableOpacity>
-                            {showStartPicker && (
-                                <DatePicker
-                                    modal
-                                    mode="date"
-                                    open={showStartPicker}
-                                    date={startDate}
-                                    onConfirm={(date) => {
-                                        setShowStartPicker(false);
-                                        setStartDate(date);
-                                    }}
-                                    onCancel={() => setShowStartPicker(false)}
-                                />
-                            )}
-                        </View>
-
-                        <Icon name="arrow-forward" size={30} color="#000" style={styles.arrowIcon} />
-
-                        <View style={styles.datePickerWrapper}>
-                            {/* <Text style={styles.datePickerLabel}>End</Text> */}
-                            <TouchableOpacity onPress={() => setShowEndPicker(true)} style={styles.datePickerButton}>
-                                <Text style={styles.datePickerButtonText}>{formatDate(endDate)}</Text>
-                            </TouchableOpacity>
-                            {showEndPicker && (
-                                <DatePicker
-                                    modal
-                                    mode="date"
-                                    open={showEndPicker}
-                                    date={endDate}
-                                    onConfirm={(date) => {
-                                        setShowEndPicker(false);
-                                        setEndDate(date);
-                                    }}
-                                    onCancel={() => setShowEndPicker(false)}
-                                />
-                            )}
-                        </View>
-                    </View>
-                </View>
-                <View style={styles.infoContainer}>
-                    <Text style={styles.infoTitle}>Informasi Mobil</Text>
-                    <View style={styles.infoWrapper}>
-                        <View style={styles.info}>
-                            <Icon name="settings" size={20} color="#D3D3D3" />
-                            <Text style={styles.infoText}>{car.transmission}</Text>
-                        </View>
-                        <View style={styles.info}>
-                            <Icon name="people" size={20} color="#D3D3D3" />
-                            <Text style={styles.infoText}>{car.passenger}</Text>
-                        </View>
-                        <View style={styles.info}>
-                            <Icon name="local-gas-station" size={20} color="#D3D3D3" />
-                            <Text style={styles.infoText}>{car.oil}</Text>
-                        </View>
-                        {car.driver ? <View style={styles.info}>
-                            <Icon name="person" size={20} color="#32CD32" />
-                            <Text style={styles.infoDriver}> Sopir</Text>
-                        </View> : null}
-                    </View>
-                </View>
-                <Text style={styles.aboutTitle}>About Car</Text>
-                <Text style={styles.aboutText}>{car.about}</Text>
-                <View style={styles.bottomContainer}>
-                    <Text style={styles.totalPriceText}>Total Harga: Rp {formatPrice(totalPrice)}</Text>
-                    <TouchableOpacity style={styles.rentButton} onPress={handleRentCar}>
-                        <Text style={styles.rentButtonText}>Sewa Sekarang</Text>
-                    </TouchableOpacity>
-                </View>
-                <Modal
-                    visible={showLoginModal}
-                    transparent
-                    animationType="slide"
-                    onRequestClose={() => setShowLoginModal(false)}
-                >
-                    <View style={styles.modalContainer}>
-                        <View style={styles.modalContent}>
-                            <Text style={styles.modalText}>Anda belum login</Text>
-                            <TouchableOpacity
-                                style={styles.modalButton}
-                                onPress={() => {
-                                    setShowLoginModal(false);
-                                    navigation.navigate('Login');
-                                }}
-                            >
-                                <Text style={styles.modalButtonText}>Login</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                </Modal>
-                <Modal
-                    visible={showConfirmModal}
-                    transparent
-                    animationType="slide"
-                    onRequestClose={() => setShowConfirmModal(false)}
-                >
-                    <View style={styles.modalContainer}>
-                        <View style={styles.modalContent}>
-                            <Text style={styles.modalText}>Apakah Anda yakin ingin menyewa mobil ini?</Text>
-                            <View style={styles.modalButtonContainer}>
-                                <TouchableOpacity
-                                    style={styles.modalButton}
-                                    onPress={() => setShowConfirmModal(false)}
-                                >
-                                    <Text style={styles.modalButtonText}>Batal</Text>
+            <ScrollView style={styles.scrollContainer}>
+                <View style={styles.wrapper}>
+                    <Text style={styles.name}>{car.nameCar}</Text>
+                    <View style={styles.datePickerSection}>
+                        <Text style={styles.datePickerSectionText} >Atur tanggal sewa</Text>
+                        <View style={styles.datePickerRow}>
+                            <View style={styles.datePickerWrapper}>
+                                <TouchableOpacity onPress={() => setShowStartPicker(true)} style={styles.datePickerButton}>
+                                    <Text style={styles.datePickerButtonText}>{formatDate(startDate)}</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity
-                                    style={[styles.modalButton, styles.confirmButton]}
-                                    onPress={confirmRentCar}
-                                >
-                                    <Text style={styles.modalButtonText}>Ya</Text>
+                                {showStartPicker && (
+                                    <DatePicker
+                                        modal
+                                        mode="date"
+                                        open={showStartPicker}
+                                        date={startDate}
+                                        onConfirm={(date) => {
+                                            setShowStartPicker(false);
+                                            setStartDate(date);
+                                        }}
+                                        onCancel={() => setShowStartPicker(false)}
+                                    />
+                                )}
+                            </View>
+
+                            <Icon name="arrow-forward" size={30} color="#000" style={styles.arrowIcon} />
+
+                            <View style={styles.datePickerWrapper}>
+                                <TouchableOpacity onPress={() => setShowEndPicker(true)} style={styles.datePickerButton}>
+                                    <Text style={styles.datePickerButtonText}>{formatDate(endDate)}</Text>
                                 </TouchableOpacity>
+                                {showEndPicker && (
+                                    <DatePicker
+                                        modal
+                                        mode="date"
+                                        open={showEndPicker}
+                                        date={endDate}
+                                        onConfirm={(date) => {
+                                            setShowEndPicker(false);
+                                            setEndDate(date);
+                                        }}
+                                        onCancel={() => setShowEndPicker(false)}
+                                    />
+                                )}
                             </View>
                         </View>
                     </View>
-                </Modal>
+                    <View style={styles.infoContainer}>
+                        <Text style={styles.infoTitle}>Informasi Mobil</Text>
+                        <View style={styles.infoWrapper}>
+                            <View style={styles.info}>
+                                <Icon name="settings" size={20} color="#D3D3D3" />
+                                <Text style={styles.infoText}>{car.transmission}</Text>
+                            </View>
+                            <View style={styles.info}>
+                                <Icon name="people" size={20} color="#D3D3D3" />
+                                <Text style={styles.infoText}>{car.passenger}</Text>
+                            </View>
+                            <View style={styles.info}>
+                                <Icon name="local-gas-station" size={20} color="#D3D3D3" />
+                                <Text style={styles.infoText}>{car.oil}</Text>
+                            </View>
+                            {car.driver ? <View style={styles.info}>
+                                <Icon name="person" size={20} color="#32CD32" />
+                                <Text style={styles.infoDriver}> Sopir</Text>
+                            </View> : null}
+                        </View>
+                    </View>
+                    <Text style={styles.aboutTitle}>About Car</Text>
+                    <Text style={styles.aboutText}>{car.about}</Text>
+                </View>
+            </ScrollView>
+            <View style={styles.bottomContainer}>
+                <Text style={styles.totalPriceText}>Total Harga: Rp {formatPrice(totalPrice)}</Text>
+                <TouchableOpacity style={styles.rentButton} onPress={handleRentCar}>
+                    <Text style={styles.rentButtonText}>Sewa Sekarang</Text>
+                </TouchableOpacity>
             </View>
+            <Modal
+                visible={showLoginModal}
+                transparent
+                animationType="slide"
+                onRequestClose={() => setShowLoginModal(false)}
+            >
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalContent}>
+                        <Text style={styles.modalText}>Anda belum login</Text>
+                        <TouchableOpacity
+                            style={styles.modalButton}
+                            onPress={() => {
+                                setShowLoginModal(false);
+                                navigation.navigate('Login');
+                            }}
+                        >
+                            <Text style={styles.modalButtonText}>Login</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
+            <Modal
+                visible={showConfirmModal}
+                transparent
+                animationType="slide"
+                onRequestClose={() => setShowConfirmModal(false)}
+            >
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalContent}>
+                        <Text style={styles.modalText}>Apakah Anda yakin ingin menyewa mobil ini?</Text>
+                        <View style={styles.modalButtonContainer}>
+                            <TouchableOpacity
+                                style={styles.modalButton}
+                                onPress={() => setShowConfirmModal(false)}
+                            >
+                                <Text style={styles.modalButtonText}>Batal</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={[styles.modalButton, styles.confirmButton]}
+                                onPress={confirmRentCar}
+                            >
+                                <Text style={styles.modalButtonText}>Ya</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
         </View>
     );
 };
@@ -220,9 +220,11 @@ const styles = StyleSheet.create({
         resizeMode: 'contain',
         marginBottom: 20,
     },
-    wrapper: {
+    scrollContainer: {
         flex: 1,
-        backgroundColor: '#fff',
+        marginBottom: 50,
+    },
+    wrapper: {
         padding: 20,
     },
     name: {
@@ -249,12 +251,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-between',
         marginVertical: 10,
-    },
-    datePickerLabel: {
-        fontSize: 18,
-        marginBottom: 10,
-        color: '#000',
-        textAlign: 'center',
     },
     datePickerWrapper: {
         flex: 1,
@@ -311,8 +307,10 @@ const styles = StyleSheet.create({
         color: '#000',
     },
     bottomContainer: {
+        paddingTop: 10,
+        backgroundColor: '#fff',
         position: 'absolute',
-        bottom: 20,
+        bottom: 15,
         left: 20,
         right: 20,
         flexDirection: 'row',
